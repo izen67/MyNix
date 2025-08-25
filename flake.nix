88f56b1803515e37803da7524b6d241a21cbdf68
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-25 13:56:55 +0300

    remove spicetify

--- flake.nix
+++ flake.nix
@@ -4,25 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, spicetify-nix, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       # specialArgs makes `inputs` available to every module
       specialArgs = { inherit inputs; };
 
       modules = [
         ./configuration.nix
-        ./modules/spicetify.nix
         home-manager.nixosModules.default
       ];
     };
